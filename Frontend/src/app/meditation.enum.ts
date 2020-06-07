4741a19ca0e029325674f3db97eade18e53cea87
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-07 12:04:42 +0200

    create enum files for meditation and yoga objects

---
+++ Frontend/src/app/meditation.enum.ts
@@ -0,0 +1,11 @@
+export enum Meditation {
+    'Méditation sur les sons',
+    'Méditation sur les émotions',
+    'Méditation sur les sensations',
+    'Méditation active',
+    'Méditation vipassana',
+    'Méditation sur la compassion',
+    'Méditation assise',
+    'Méditation sur le souffle',
+    'Scan Corporel'
+  }
---
+++ Frontend/src/app/yoga.enum.ts
@@ -0,0 +1,4 @@
+export enum sessionYoga {
+    'méditation guidée',
+    'Pranayama'
+  }
