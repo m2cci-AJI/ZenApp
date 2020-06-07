c16e30fb6e4fc32d5e7355c8ec8eba0b1c1586b8
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-07 11:57:24 +0200

    create comment model used to instantate a comment object

---
+++ Frontend/src/app/models/comment.model.ts
@@ -0,0 +1,10 @@
+export class Comment {
+    sensations: string;
+    emotions: string;
+    pensees: string;
+    constructor(sensations: string, emotions: string, pensees: string) {
+       this.sensations = sensations;
+       this.emotions = emotions;
+       this.pensees = pensees;
+    }
+  }
