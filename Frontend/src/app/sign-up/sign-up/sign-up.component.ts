961d63310f4be910b783f517e16f93b6316a26a5
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-08 18:02:50 +0200

    add css styles and html content to signup page

--- Frontend/src/app/sign-up/sign-up/sign-up.component.ts
+++ Frontend/src/app/sign-up/sign-up/sign-up.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Yogi } from 'src/app/models/yogi.model';
+import { YogiService } from 'src/app/services/yogi.service';
+import { Router } from '@angular/router';
+import { MustMatch } from './must-match.validator';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+  messageError: string;
+  constructor(private formBuilder: FormBuilder,
+              private yogiService: YogiService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      login: ['', Validators.required],
+      birthday: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      MPasse: ['', [Validators.required, Validators.minLength(8)]],
+      CMPasse: ['', [Validators.required]]
+    }, {
+      validator: MustMatch('MPasse', 'CMPasse')
+    });
   }
+
+  onSubmit() {
+    const name = this.contactForm.value.name;
+    const login = this.contactForm.value.login;
+    const birthday = this.contactForm.value.birthday;
+    const email = this.contactForm.value.email;
+    const password = this.contactForm.value.MPasse;
+    const ConnectionPerWeek = 0;
+    const yogi = new Yogi(name, login, birthday, email, password, ConnectionPerWeek);
+    this.yogiService.addYogi(yogi).subscribe(
+      ( ) => {
+        this.router.navigateByUrl('/login');
+      },
+      (err) => {
+        console.log(err);
+        this.messageError = err.error.message;
+      }
+    );
+  }
+
+  onGoBack() {
+    this.contactForm.reset();
+    this.messageError = null;
+  }
+
+  validForm() {
+    return this.contactForm.status === 'INVALID';
+  }
+
 
 }
