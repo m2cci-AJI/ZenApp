25d1975d5471dd9644ca7b2c6192fa31125b8613
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-08 11:58:40 +0200

    add css styles and html content of popup window for showing comments associated to meditation or pranayama previous session

--- Frontend/src/app/calendrier/window-comment/window-comment.component.ts
+++ Frontend/src/app/calendrier/window-comment/window-comment.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CalendrierService } from 'src/app/services/calendrier.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-window-comment',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./window-comment.component.css']
 })
 export class WindowCommentComponent implements OnInit {
+  sensations: string;
+  emotions: string;
+  pensees: string;
 
-  constructor() { }
+  constructor(private dialogRef: MatDialogRef<WindowCommentComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              private calendrierService: CalendrierService) {
+                const idyogi = new JwtHelperService().decodeToken(localStorage.getItem('token')).id;
+                this.calendrierService.getItems(idyogi).subscribe((outputs) => {
+                   const sessionsYoga = ((outputs.body)as any).Data;
+                   const sessionYoga = sessionsYoga[data.item - 1];
+                   this.sensations = sessionYoga.comment.sensations;
+                   this.emotions = sessionYoga.comment.emotions;
+                   this.pensees = sessionYoga.comment.pensees;
+                }, (err) => {
+                    console.log('the error is: ', err);
+                });
+
+  }
 
   ngOnInit() {
+  }
+
+  closeModal() {
+    this.dialogRef.close();
   }
 
 }
