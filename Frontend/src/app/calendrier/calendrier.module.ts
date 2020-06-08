a95588f9a8259af09d1fa9672afd2af44250cf3f
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-08 11:54:59 +0200

    create css styles and html content of the popup window for settuing the current visible timeline

--- Frontend/src/app/calendrier/calendrier.module.ts
+++ Frontend/src/app/calendrier/calendrier.module.ts
@@ -5,12 +5,16 @@ import { WindowCommentComponent } from './window-comment/window-comment.componen
 import { WindowTimeComponent } from './window-time/window-time.component';
 import { calendrierRoutes } from './calendrier.routes';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [TimelineComponent, WindowCommentComponent, WindowTimeComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(calendrierRoutes)
+    RouterModule.forChild(calendrierRoutes),
+    FormsModule,
+    NgbModule
   ]
 })
 export class CalendrierModule { }
--- Frontend/src/app/calendrier/window-time/window-time.component.ts
+++ Frontend/src/app/calendrier/window-time/window-time.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-window-time',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./window-time.component.css']
 })
 export class WindowTimeComponent implements OnInit {
+  dateStart: Date;
+  dateEnd: Date;
 
-  constructor() { }
+  constructor(private dialogRef: MatDialogRef<WindowTimeComponent>) { }
 
   ngOnInit() {
+  }
+
+  onWindow(f) {
+    this.dateStart = f.value['dp3'];
+    this.dateEnd = f.value['dp4'];
+    this.dialogRef.close({action: 1, data: {start: this.dateStart, end: this.dateEnd}});
+  }
+
+  closeModal() {
+    this.dialogRef.close();
+  }
+
+  CompareDates(dateStart: any, dateEnd: any) {
+    return (dateStart > dateEnd);
   }
 
 }
