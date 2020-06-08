d881fdc635808b60e85f1dc9502d623c400f3b39
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-08 10:33:23 +0200

    add css styles and html content on the practice subpage of the meditation page

--- Frontend/src/app/meditation/practice/practice.component.ts
+++ Frontend/src/app/meditation/practice/practice.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MeditationService } from '../../services/meditation.service';
+import { CalendrierService } from 'src/app/services/calendrier.service';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CommentComponent } from 'src/app/comment/comment.component';
+import { Yoga } from 'src/app/models/yoga.model';
+import { sessionYoga } from 'src/app/yoga.enum';
+import { Comment } from 'src/app/models/comment.model';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 
 @Component({
   selector: 'app-practice',
@@ -6,10 +15,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./practice.component.css']
 })
 export class PracticeComponent implements OnInit {
-
-  constructor() { }
+  meditations: any;
+  audioObject = new Audio();
+  comment: Comment;
+  index: number;
+  idSession: any = undefined;
+  sousTypeYoga: any;
+  constructor(private meditationService: MeditationService,
+              private calendrierService: CalendrierService,
+              private matDialog: MatDialog) { }
 
   ngOnInit() {
+    this.meditations = [];
+    this.meditationService.getMeditations();
+    this.meditations = this.meditationService.meditations;
+  }
+
+  audioEnded(index: number) {
+    this.comment = new Comment('', '', '');
+    this.index = index;
+    this.editComment();
+  }
+
+  creationYogaCalendrier() {
+    const meditation = this.meditations[this.index];
+    const typeYoga = sessionYoga['0'];
+    const sousTypeYoga = meditation.typeMeditation;
+    const durationYoga = meditation.durationSession;
+    const end = new Date();
+    const start = this.calendrierService.getStart(end, durationYoga);
+    const comment = this.comment;
+    const img = meditation.image;
+    const idYogi = new JwtHelperService().decodeToken(localStorage.getItem('token')).id;
+    const sessionyoga = new Yoga(idYogi, typeYoga, sousTypeYoga, durationYoga, start, end, comment, img);
+    if ((this.idSession === undefined) ||  (this.sousTypeYoga !== sousTypeYoga)) {
+      this.calendrierService.addItem(sessionyoga).subscribe((data) => {
+        console.log('session added with success !, ', data.body);
+        this.idSession = ((data.body) as any).Data._id;
+        this.sousTypeYoga = sousTypeYoga;
+      },
+      (err) => {
+        console.log('the error is: ', err);
+      });
+    } else if ((this.idSession !== undefined) && (this.sousTypeYoga === sousTypeYoga)) {
+        this.calendrierService.updateItem(sessionyoga, this.idSession).subscribe(() => {
+           console.log('session updates with success !');
+      },
+      (err) => {
+           console.log('the error is: ', err);
+      });
+    }
+  }
+
+  endAudio(index: number) {
+      if (index === this.index) {
+        return true;
+      } else {
+        return false;
+      }
+  }
+
+  openModal(dialogConfig: MatDialogConfig) {
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    if (screen.width <= 600) {
+      dialogConfig.width = '99%';
+    } else {
+      dialogConfig.width = '50%';
+    }
+    if (this.comment !== undefined) {
+      dialogConfig.data = {comment: this.comment, value: true};
+    }
+  }
+
+  editComment() {
+    const dialogConfig = new MatDialogConfig();
+    this.openModal(dialogConfig);
+    const dialogRef = this.matDialog.open(CommentComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe(
+      (data) => {
+        if (data) {
+          if (data.action === 1) {
+            this.comment = data.data;
+            this.creationYogaCalendrier();
+          }
+        }
+      }
+    );
   }
 
 }
