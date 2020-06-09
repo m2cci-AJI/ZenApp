ce4be396a2829c6a1710b7c6b9dc907123370da7
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-09 22:27:34 +0200

    create yogi model used to stock user properties on mongodb database

---
+++ Backend/src/models/yogi.model.ts
@@ -0,0 +1,16 @@
+import { Schema, Model, model } from "mongoose";
+let uniqueValidator = require('mongoose-unique-validator');
+
+
+let yogiSchema: Schema = new Schema({
+  name: {type: String, required: true},
+  login: {type: String, required: true},
+  birthday: {type: String, required: true},
+  email: {type: String, required: true, unique:true},
+  password: {type: String, required: true},
+  ConnectionPerWeek: {type: Number, required: true}
+});
+
+yogiSchema.plugin(uniqueValidator);
+
+export const Yogi: Model<any> = model<any>("Yogi", yogiSchema);
