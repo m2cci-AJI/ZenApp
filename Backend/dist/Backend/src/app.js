ebb2067d398ca67468424b57c791efc1eeb455e4
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-10 17:07:57 +0200

    create middleware used to sign up user

--- Backend/dist/Backend/src/app.js
+++ Backend/dist/Backend/src/app.js
@@ -5,6 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var mongoose = require("mongoose");
+var bodyparser = require("body-parser");
+var cors_1 = __importDefault(require("cors"));
+var yogi_model_1 = require("./models/yogi.model");
+var bcrypt = require('bcryptjs');
 var Application = /** @class */ (function () {
     function Application(port) {
         this.port = port;
@@ -12,16 +16,39 @@ var Application = /** @class */ (function () {
     Application.prototype.start = function () {
         var app = express_1.default();
         var MONGODB_CONNECTION = 'mongodb://127.0.0.1:27017';
+        app.use(cors_1.default());
+        app.options('*', cors_1.default());
+        app.use(bodyparser.json());
+        app.use(bodyparser.urlencoded({ limit: '10mb', extended: true }));
+        var SECRET_KEY = 'aagethrud812d8d2dhdydbd5d4d2d';
         mongoose.connect(MONGODB_CONNECTION, {
             useNewUrlParser: true
         });
         var db = mongoose.connection;
-        console.log('oppps');
         db.once('open', function (_) {
             console.log('Database connected:', MONGODB_CONNECTION);
         });
         db.on('error', function (err) {
             console.error('connection error:', err);
+        });
+        app.post('/api/signup', function (req, res, next) {
+            bcrypt.hash(req.body.password, 10)
+                .then(function (hash) {
+                var yogi = new yogi_model_1.Yogi({
+                    login: req.body.login,
+                    name: req.body.name,
+                    email: req.body.email,
+                    birthday: req.body.birthday,
+                    password: hash,
+                    ConnectionPerWeek: req.body.ConnectionPerWeek
+                });
+                yogi.save()
+                    .then(function (data) { res.status(200).json({ message: 'user is successifuly added !', Data: data }); })
+                    .catch(function (err) {
+                    res.status(400).json({ message: 'L\'email que vous saisissez est invalide. Cet email existe déja !', err: err });
+                })
+                    .catch(function (err) { res.status(500).json({ message: 'Cette erreur vient du serveur ! Veuillez re-connecter ultérieurement.', error: err }); });
+            });
         });
         app.listen(this.port, function () {
             console.log("réponse recue !");
--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,6 +1,10 @@
 
 import express, { Request, Response } from 'express';
 import mongoose = require("mongoose");
+import bodyparser = require("body-parser");
+import cors from 'cors';
+import { Yogi } from './models/yogi.model';
+var bcrypt = require('bcryptjs');
 
 export class Application {
 
@@ -9,18 +13,44 @@ export class Application {
     start() {
         const app = express();
         const MONGODB_CONNECTION: string = 'mongodb://127.0.0.1:27017';
+        app.use(cors());
+        app.options('*', cors());
+        app.use(bodyparser.json());
+        app.use(bodyparser.urlencoded({ limit: '10mb', extended: true }));
+        const SECRET_KEY: string = 'aagethrud812d8d2dhdydbd5d4d2d';
         mongoose.connect(MONGODB_CONNECTION,
             {
                 useNewUrlParser: true
             });
         const db = mongoose.connection;
-        console.log('oppps');
+        
         db.once('open', _ => {
             console.log('Database connected:', MONGODB_CONNECTION)
         });
         db.on('error', err => {
             console.error('connection error:', err)
         });
+
+        app.post('/api/signup', (req: Request, res: Response, next) => {
+            bcrypt.hash(req.body.password, 10)
+                .then((hash:any) => {
+                    const yogi = new Yogi({
+                        login: req.body.login,
+                        name: req.body.name,
+                        email: req.body.email,
+                        birthday: req.body.birthday,
+                        password: hash,
+                        ConnectionPerWeek: req.body.ConnectionPerWeek
+                    });
+                    yogi.save()
+                        .then((data: any) => { res.status(200).json({ message: 'user is successifuly added !', Data: data }) })
+                        .catch((err: any) => {
+                            res.status(400).json({ message: 'L\'email que vous saisissez est invalide. Cet email existe déja !', err: err });
+                        })
+                        .catch((err: any) => { res.status(500).json({ message: 'Cette erreur vient du serveur ! Veuillez re-connecter ultérieurement.', error: err }) });
+                });
+        });
+
         app.listen(this.port, () => {
             console.log("réponse recue !");
         });
