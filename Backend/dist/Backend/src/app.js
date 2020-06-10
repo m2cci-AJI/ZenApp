565f314962f248eb4391530101d46c7d59ba4008
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-10 08:33:24 +0200

    create the connection with the mongodb database and verify if the application is connected or not

--- Backend/dist/Backend/src/app.js
+++ Backend/dist/Backend/src/app.js
@@ -4,12 +4,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var mongoose = require("mongoose");
 var Application = /** @class */ (function () {
     function Application(port) {
         this.port = port;
     }
     Application.prototype.start = function () {
         var app = express_1.default();
+        var MONGODB_CONNECTION = 'mongodb://127.0.0.1:27017';
+        mongoose.connect(MONGODB_CONNECTION, {
+            useNewUrlParser: true
+        });
+        var db = mongoose.connection;
+        console.log('oppps');
+        db.once('open', function (_) {
+            console.log('Database connected:', MONGODB_CONNECTION);
+        });
+        db.on('error', function (err) {
+            console.error('connection error:', err);
+        });
         app.listen(this.port, function () {
             console.log("réponse recue !");
         });
--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,5 +1,6 @@
 
 import express, { Request, Response } from 'express';
+import mongoose = require("mongoose");
 
 export class Application {
 
@@ -7,6 +8,19 @@ export class Application {
 
     start() {
         const app = express();
+        const MONGODB_CONNECTION: string = 'mongodb://127.0.0.1:27017';
+        mongoose.connect(MONGODB_CONNECTION,
+            {
+                useNewUrlParser: true
+            });
+        const db = mongoose.connection;
+        console.log('oppps');
+        db.once('open', _ => {
+            console.log('Database connected:', MONGODB_CONNECTION)
+        });
+        db.on('error', err => {
+            console.error('connection error:', err)
+        });
         app.listen(this.port, () => {
             console.log("réponse recue !");
         });
