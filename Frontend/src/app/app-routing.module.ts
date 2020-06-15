8a05e3ad50236f402e39b64a4077ffb43acd63ac
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-15 14:54:51 +0200

    create unit tests for header.component

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,27 +1,7 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { paranayamaRoutes } from './pranayama/pranayama.routes';
-import { homeRoute } from './home/home.route';
-import { meditationRoutes } from './meditation/meditation.routes';
-import { calendrierRoutes } from './calendrier/calendrier.routes';
-import { statisticsRoutes } from './statistics/statistics.routes';
-import { signupRoutes } from './sign-up/sign-up.routes';
-import { loginRoutes } from './login/login.routes';
-import { requestresetpasswordRoute } from './request-reset-password/request-reset-password.route';
-import { responseresetpasswordRoute } from './response-reset-password/response-reset-password.route';
+import { RouterModule } from '@angular/router';
+import { routes } from './routes.app';
 
-const routes: Routes = [
-  requestresetpasswordRoute,
-  responseresetpasswordRoute,
-  homeRoute,
-  {path: '', redirectTo: 'home' , pathMatch: 'full'},
-  ...paranayamaRoutes,
-  ...meditationRoutes,
-  ...calendrierRoutes,
-  ...statisticsRoutes,
-  ...signupRoutes,
-  ...loginRoutes
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
