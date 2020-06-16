24402e8de2c7de4fd9679fb36f39b86ce1235e20
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-16 10:10:58 +0200

    create unit tests for duration-breathing.component

--- Frontend/src/test.ts
+++ Frontend/src/test.ts
@@ -15,6 +15,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('src/app/meditation/practice', true, /\.spec\.ts$/);
+const context = require.context('src/app/pranayama/tuning/duration-breathing', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
