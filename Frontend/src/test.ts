f59dd316830a13ebbdd34ddfce4a97220cd8b690
jemaiAH <ahmed.jemai@hotmail.com>
2020-06-16 13:13:27 +0200

    create unit tests for start-tuning.component

--- Frontend/src/test.ts
+++ Frontend/src/test.ts
@@ -15,6 +15,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('src/app/pranayama/tuning/musique-tuning', true, /\.spec\.ts$/);
+const context = require.context('src/app/pranayama/tuning/start-tuning', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
